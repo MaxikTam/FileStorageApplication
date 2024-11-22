34affbb01b9ebd18f644d589b6f66c426a340f4d
Maxim Tamoshkin <Tamoshkin.M@nppstels.ru>
2024-11-22 16:18:09 +0700

    Модель не доделать

--- LogicApp/Controllers/AdminController.cs
+++ LogicApp/Controllers/AdminController.cs
@@ -7,8 +7,35 @@ public class AdminController : Controller
 {
     [HttpGet]
     [Authorize(Roles = "Admin")]
+    [Route("")]
     public IActionResult Index()
     {
         return View();
     }
+    
+    [HttpGet]
+    [Route("Filters")]
+    public IActionResult Filters()
+    {
+        return View();
+    }
+    
+    [Route("Storage")]
+    public IActionResult Storage()
+    {
+        return View();
+    }
+    
+    [Route("Statistic")]
+    public IActionResult Statistic()
+    {
+        return View();
+    }
+    
+    [Route("Archive")]
+    public IActionResult Archive()
+    {
+        return View();
+    }
+    
 }
---
+++ LogicApp/Views/Admin/Filters.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using LogicApp.Models.Enum;
+
+namespace LogicApp.Views.Admin;
+
+public class Filters_cshtml
+{
+    /// <summary>
+    /// По умолчанию, нельзя загружать файлы, размером более 20 Мб.
+    /// </summary>
+    private const double Default = 20;
+
+    public Dictionary<TypeFile, long> ExtensionSize { get; set; } = new();
+    
+    /// <summary>
+    /// Разрешённые расширения в строковом виде.
+    /// </summary>
+    public string Extensions
+    {
+        get
+        {
+            var result = new StringBuilder();
+            foreach (var type in ExtensionSize.Keys)
+            {
+                result.Append('.');
+                result.Append('.');
+                result.Append(type.ToString().ToLower());
+                result.Append(',');
+                result.Append(' ');
+            }
+
+            return result.ToString().TrimEnd(',');    
+        }
+    }
+
+    /// <summary>
+    /// Ограничение размера файла в Mb
+    /// </summary>
+    public double GetExtensionSizeLimit(TypeFile typeFile)
+    {
+        if (!ExtensionSize.TryGetValue(typeFile, out var size))
+        {
+            return Default;
+        }
+        return (double)ExtensionSize[typeFile] / (1024 * 1024);
+    }
+
+}
